24cf4dfca0969d634ee20ddc5c0968656ead7959
Ru Al <39805456+rualb@users.noreply.github.com>
2019-09-15 22:49:39 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='my_tools',
+    packages=['my_tools'],
+    description='General Tools',
+    version='1.0.0',
+    url='https://github.com/rualb/my_py',
+    author='rualb',
+    #author_email='rualb@....com',
+    keywords=['my_tools']
+    )
+    
